134fb365036b2ada8d7ad85e56e43de0c3477daa
Marcin Paczkowski <paczkowski.marcin@icloud.com>
2020-04-27 20:11:53 +0200

    Refactoring

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Layout from '../components/Layout/layout';
-import Zones from '../components/Zones/zones';
 import '../styles/style.scss';
 
 const IndexPage = () => (
   <Layout>
-    <Zones />
+    <div style={{ height: '1000px' }}>Test</div>
   </Layout>
 );
 
