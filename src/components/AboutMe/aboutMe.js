aa9e5be920974aec0ee17191faae80c6a2e41fc3
Marcin Paczkowski <paczkowski.marcin@icloud.com>
2020-06-12 22:53:04 +0200

    Add about me title

--- src/components/AboutMe/aboutMe.js
+++ src/components/AboutMe/aboutMe.js
@@ -5,6 +5,7 @@ import AboutMeContent from './AboutMeContent/aboutMeContent';
 const AboutMe = ({ articles }) => {
   return (
     <div className="about-me">
+      <h1 className="about-me__title">O mnie</h1>
       <div className="about-me__content">
         <AboutMeContent content={articles}></AboutMeContent>
       </div>
