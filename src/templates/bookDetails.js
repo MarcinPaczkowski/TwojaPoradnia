3a3932abe76626c014ed5d79eebde605d396fc96
Marcin Paczkowski <paczkowski.marcin@icloud.com>
2020-08-12 22:50:12 +0200

    Fix

--- src/templates/bookDetails.js
+++ src/templates/bookDetails.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Layout from '../components/Layout/layout';
 import '../styles/style.scss';
 import { buildBreadcrumbs } from '../utils/breadcrumbsHelpers';
-import { mapCmsBook } from '../utils/cmsMappers/booksMapper';
+// import { mapCmsBook } from '../utils/cmsMappers/booksMapper';
 
 const BookDetails = pageData => {
   const breadcrumbs = buildBreadcrumbs(pageData, 'Książki');
-  const bookDetails = mapCmsBook(pageData.pageContext);
+  // const bookDetails = mapCmsBook(pageData.pageContext);
   return (
     <Layout breadcrumbs={breadcrumbs}>
       <p>Szczegóły</p>
