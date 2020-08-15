de4a85bddb7d26eab922dca28ce44eb3d1d97fb8
Marcin Paczkowski <paczkowski.marcin@icloud.com>
2020-08-15 22:10:36 +0200

    Refactoring

--- src/components/Shared/Tile/TileContent/tileContent.js
+++ src/components/Shared/Tile/TileContent/tileContent.js
@@ -15,7 +15,7 @@ const TileContent = ({
   return (
     <div className="tile-content">
       <div className="tile-content__wrapper">
-        <h2 className="tile-content__wrapper--title">{title}</h2>
+        <h2>{title}</h2>
         {parse(description)}
       </div>
       <div className="tile-content__link">
