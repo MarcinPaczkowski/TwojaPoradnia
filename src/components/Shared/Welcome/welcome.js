f1979444f7b174cbec68a1cf140ba5c2a615748e
Marcin Paczkowski <paczkowski.marcin@icloud.com>
2020-08-27 14:00:33 +0200

    Minor fixes

--- src/components/Shared/Welcome/welcome.js
+++ src/components/Shared/Welcome/welcome.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Parallax } from 'react-scroll-parallax';
 import GatsbyImage from 'gatsby-image/withIEPolyfill';
 import './welcome.scss';
 import parse from 'html-react-parser';
 
 const Welcome = ({ welcome: { title, description, image } }) => {
+  useEffect(() => {
+    function handleResize() {
+      window.document.getElementsByClassName(
+        'parallax-inner'
+      )[0].style.transform = 'translate3d(0%, 0%, 0px)';
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className="welcome">
       <Parallax className="welcome__background" y={[-50, 50]} tagOuter="figure">
         <GatsbyImage
           className="welcome__background--image"
           objectFit="cover"
-          objectPosition="50% 30%"
+          objectPosition="15% 30%"
           fluid={image.fluid}
           alt={image.description}
         ></GatsbyImage>
       </Parallax>
+      <div className="welcome__content-background"></div>
       <div className="welcome__content">
-        <div className="welcome-content__background"></div>
         <div className="welcome-content__title welcome-content__text">
           {parse(title)}
         </div>
