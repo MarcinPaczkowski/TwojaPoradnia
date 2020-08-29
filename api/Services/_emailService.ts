2afa359cf8db3dbe009c07c3660a7281c827942c
Marcin Paczkowski <paczkowski.marcin@icloud.com>
2020-08-29 05:52:29 +0200

    WIP

--- api/Services/_emailService.ts
+++ api/Services/_emailService.ts
@@ -1,15 +1,41 @@
 import * as nodemailer from 'nodemailer';
+import { google } from 'googleapis';
+const { OAuth2 } = google.auth;
 
 export class EmailService {
+  private clientId: string = process.env.MAILING_SERVICE_CLIENT_ID;
+  private clientSecret: string = process.env.MAILING_SERVICE_CLIENT_SECRET;
+  private refreshToken: string = process.env.MAILING_SERVICE_REFRESH_TOKEN;
   private transporter: nodemailer.Transporter;
   private user: string = process.env.emailUser;
-  private password: string = process.env.emailPassword;
+  // private password: string = process.env.emailPassword;
   private recipient: string = process.env.emailRecipient;
+  private OAUTH_PLAYGROUND: string =
+    'https://developers.google.com/oauthplayground';
 
   constructor() {
-    this.transporter = nodemailer.createTransport(
-      `smtps://${this.user}@gmail.com:${this.password}@smtp.gmail.com`
+    const oauth2Client = new OAuth2(
+      this.clientId,
+      this.clientSecret,
+      this.OAUTH_PLAYGROUND
     );
+
+    oauth2Client.setCredentials({
+      refresh_token: this.refreshToken,
+    });
+    const accessToken = oauth2Client.getAccessToken();
+
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: this.user,
+        clientId: this.clientId,
+        clientSecret: this.clientSecret,
+        refreshToken: this.refreshToken,
+        accessToken,
+      },
+    });
   }
 
   async sendMail(subject: string, content: string) {
