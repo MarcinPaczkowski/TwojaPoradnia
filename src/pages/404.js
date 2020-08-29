4097aa4e862de84128dd256d6275f9b74785fa4c
Marcin Paczkowski <paczkowskimarcin@outlook.com>
2020-08-29 03:52:14 +0200

    Feature/not found page (#45)
    * 404
    
    * NotFound component
    
    * Counter to redirect

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,6 @@
 import React from 'react';
+import NotFound from '../components/NotFound/notFound';
 
-const NotFoundPage = () => (
-  <>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-);
+const NotFoundPage = () => <NotFound />;
 
 export default NotFoundPage;
