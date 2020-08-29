f85e22acdfd45198a6f37629830c89262ef295aa
Marcin Paczkowski <paczkowski.marcin@icloud.com>
2020-08-29 03:51:16 +0200

    Counter to redirect

--- src/components/NotFound/notFound.js
+++ src/components/NotFound/notFound.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './notFound.scss';
 import { navigate } from 'gatsby';
+import { FaRegSadTear } from 'react-icons/fa';
 
 const NotFound = () => {
-  setTimeout(() => navigate('/'), 5000);
+  const [timeCounter, setTimeCounter] = useState(5);
+
+  useEffect(() => {
+    const timerHandler = setTimeout(
+      () => setTimeCounter(timeCounter - 1),
+      1000
+    );
+    if (timeCounter <= 0) {
+      clearTimeout(timerHandler);
+      setTimeCounter(5);
+      navigate('/');
+    }
+
+    return () => {
+      if (timerHandler) clearTimeout(timerHandler);
+    };
+  });
+
   return (
     <div className="notFound">
       <h1>
         Przykro nam, ale niestety nie znaleziono strony o podanym adresie.
       </h1>
-      <p>Za moment zostaniesz przekierowany na stronę główną</p>
+      <FaRegSadTear />
+      <p>{`Za ${timeCounter} sekund zostaniesz przekierowany na stronę główną`}</p>
     </div>
   );
 };
